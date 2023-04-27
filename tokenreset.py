d49a898e1f1c3c53cef083d04110d5b5d954e446
shaikmasthanbee <130638835+shaikmasthanbee@users.noreply.github.com>
2023-04-27 16:19:58 +0530

    Delete tokenreset.py

--- tokenreset.py
+++
@@ -1,4 +0,0 @@
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-def token(name,seconds):
-    s=Serializer('67@hjyjhk',seconds)
-    return s.dumps({'user':name}).decode('utf-8')
